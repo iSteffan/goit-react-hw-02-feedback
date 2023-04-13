44b58d3cb72ada9a96a4cb1c64e3dfac049c7bb1
Stepan Skichko <st.skichko@gmail.com>
2023-04-13 16:43:51 +0300

    functional DONE

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,101 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  // countGoodMarks = () => {
+  //   this.setState(prevState => {
+  //     console.log(prevState)
+  //     return { good: Number(prevState.value + 1), };
+  //   });
+  // };
+
+  // countGoodMarks = () => {
+  //   this.setState({good: 2 }
+  //   );
+  // };
+
+
+
+countGoodMarks = () => {
+    this.setState(oldData => {
+      let obj = { ...oldData };
+      obj.good = oldData.good + 1;
+      return obj;
+    });
+  };
+
+  countNeutralMarks = () => {
+    this.setState(oldData => {
+      let obj = { ...oldData };
+      obj.neutral = oldData.neutral + 1;
+      return obj;
+    });
+  };
+
+  countBadMarks = () => {
+    this.setState(oldData => {
+      let obj = { ...oldData };
+      obj.bad = oldData.bad + 1;
+      return obj;
+    });
+  };
+
+countTotalFeedback=()=>{return this.state.good + this.state.neutral + this.state.bad;}
+  
+countPositiveFeedbackPercentage = () => {
+    return Math.floor(
+      (this.state.good /
+        (this.state.good + this.state.neutral + this.state.bad)) *
+        100 || 0);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    
+
+    return(
+    <div>
+        <div>
+            <h2>Please leave feedback</h2>
+            <ul>
+            <li>
+              <button onClick={this.countGoodMarks}>
+                Good</button>
+            </li>
+            <li>
+              <button onClick={this.countNeutralMarks}>Neutral</button>
+            </li>
+            <li>
+              <button onClick={this.countBadMarks}>Bad</button>
+            </li>
+            </ul>
+        </div >
+        <div>
+            <h2>Statistics</h2>
+            <ul>
+                    <li>
+              <p>Good: { good}</p>
+                    </li>
+                    <li>
+              <p>Neutral: { neutral}</p>
+                    </li>
+                    <li>
+              <p>Bad: { bad}</p>
+            </li>
+            <li>
+              <p>Total: { this.countTotalFeedback()}</p>
+            </li>
+             <li>
+              <p>Positive feedback: { this.countPositiveFeedbackPercentage()}%</p>
+                    </li>
+            </ul>
+        </div >
+    </div >
   );
-};
+  }
+}
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,34 @@
+import { Component } from 'react';
+
+export class Feedback extends Component {
+
+  render() {
+    return(
+    <div>
+        <div>
+            <h2>Please leave feedback</h2>
+            <ul>
+                <li><button>Good</button></li>
+                <li><button>Neutral</button></li>
+                <li><button>Bad</button></li>
+            </ul>
+        </div >
+        <div>
+            <h2>Statistics</h2>
+            <ul>
+                    <li>
+                        <p>Good</p>
+                    </li>
+                    <li>
+                        <p>Neutral</p>
+                    </li>
+                    <li>
+                        <p>Bad</p>
+                    </li>
+            </ul>
+        </div >
+    </div >
+  );
+  }
+
+}
